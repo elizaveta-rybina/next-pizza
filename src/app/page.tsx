e13b86526917a4fc1ac92952606bf1b816155dda
elizaveta-rybina <liza.rybina@inbox.ru>
2025-01-13 23:18:00 +0300

    #fix: обновлена шапка

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from '@/components/ui/button'
+import { Container, Title, TopBar } from '@/components/shared'
 
 export default function Home() {
 	return (
 		<div className=''>
-			<Button variant='outline'>Корзина</Button>
+			<Container className='mt-10'>
+				<Title text='Все пиццы' size='lg' className='font-extrabold' />
+			</Container>
+			<TopBar />
 		</div>
 	)
 }
