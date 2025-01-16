1b5c081f8a4d70b1f7ecdc97e4de22bba634210f
elizaveta-rybina <liza.rybina@inbox.ru>
2025-01-16 22:52:58 +0300

    #feat: компонент фильтрации

---
+++ src/components/shared/filter-checkbox.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+	className?: string;
+	
+}
+
+export const FilterCheckbox: React.FC<Props> = ({ className }) => {
+	return (
+		<div className={className}>
+			
+		</div>
+	);
+};
